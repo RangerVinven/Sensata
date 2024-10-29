f40f9f320d9b49fe5e877cfb10d424ac1e9c907d
D. Hunstad <github.kc13w@codecoffin.com>
2024-10-29 14:21:04 +0000

    Add data collection endpoint.

--- api/main.py
+++ api/main.py
@@ -24,8 +24,6 @@ engine = create_engine(
     connect_args=connect_args,
 )
 
-print(SQLModel.metadata.tables)
-
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
@@ -54,3 +52,35 @@ def read_root():
 def return_all_data(session: SessionDep):
     data = session.exec(select(SensorData).limit(50)).all()
     return data
+
+
+from pydantic import BaseModel
+
+
+class sensor_data_type(BaseModel):
+    data: bytes
+    sensor_key: uuid.UUID
+    recorded_at: datetime
+
+
+@app.post("/api/v1/data")
+def add_data(json_sensor_data: sensor_data_type, session: SessionDep):
+    # create a new sensor data object
+    # but first check if the sensor key exists
+    sensor = session.exec(
+        select(SensorTable).where(SensorTable.key == json_sensor_data.sensor_key)
+    ).first()
+
+    if sensor is None:
+        raise HTTPException(status_code=404, detail="Sensor key not found")
+
+    sensor_data = SensorData(
+        data=json_sensor_data.data,
+        time_recorded=json_sensor_data.recorded_at,
+        time_added=datetime.now(),
+        sensor_id_sensor_table=sensor.sensor_id,
+        unique_id=uuid.uuid4(),
+    )
+    session.add(sensor_data)
+    session.commit()
+    return sensor_data
