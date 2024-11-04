2ca7ff1c5f413e9c711e248559512271ea00627e
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-11-04 14:26:53 +0000

    Added routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './login.tsx';
 import Dashboard from './dashboard.tsx';
 
@@ -7,8 +7,10 @@ function App() {
   return (
     // Todo - add validation so that the user can't access /dashboard without being signed in
       <BrowserRouter>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
       </BrowserRouter>
   )
 }
