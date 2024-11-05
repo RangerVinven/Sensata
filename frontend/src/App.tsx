5d55f21697d104ee78f58840de9e943e93d15457
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-11-05 13:18:02 +0000

    Installed Chakra-UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import { ChakraProvider } from "@chakra-ui/react"
+import { defaultSystem } from "@chakra-ui/react"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './login.tsx';
 import Dashboard from './dashboard.tsx';
@@ -6,12 +8,14 @@ function App() {
 
   return (
     // Todo - add validation so that the user can't access /dashboard without being signed in
+    <ChakraProvider value={defaultSystem}>
       <BrowserRouter>
         <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
+      </ChakraProvider>
   )
 }
 
