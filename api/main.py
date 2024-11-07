fcb5a0226d19e0cd8cdd21b6ccbd60227fcdcd4c
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-11-07 22:50:31 +0000

    Added a CORS configuration

--- api/main.py
+++ api/main.py
@@ -2,6 +2,7 @@ from typing import Annotated, Union
 
 from fastapi import FastAPI, Depends, HTTPException, Query, Request
 from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
 
 from sqlalchemy.util import b64decode
 from sqlmodel import Session, create_engine, select
@@ -48,6 +49,8 @@ class CustomJSONEncoder(JSONEncoder):
 
 app = FastAPI()
 
+# CORS Config
+app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 # read db url from .env file
 from dotenv import dotenv_values
