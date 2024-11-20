7569c13b0517c68a542663f4df59665db1ad15c3
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-11-20 20:47:07 +0000

    Added a call to loadDashboard() everytime a new sensor is selected

--- frontend/src/components/dashboard/SensorSelect.tsx
+++ frontend/src/components/dashboard/SensorSelect.tsx
@@ -1,19 +1,27 @@
 import SensorType from "../../assets/SensorType.tsx";
+import "../../assets/SensorSelect.css";
 
 type Props = {
     sensors: Array<SensorType> | null,
     setIsLoading: Function
+    loadDashboard: Function
 }
 
 // The select menu for the sensors
 function SensorSelect(props: Props) {
 
+    // Called when the sensor gets changed
+    function changeSensor(event: any) {
+        props.setIsLoading(true);
+        props.loadDashboard(event.target.value);
+    }
+
     return <>
-        <select className="dropdown">
+        <select className="dropdown" onChange={e => changeSensor(e)} >
             <option value="" disabled selected>Select a sensor</option>
             {
                 props.sensors?.map((sensor, index) => {
-                    return <option value={ sensor.sensor_model_name } selected={index === 0}>{ sensor.sensor_model_name }</option>
+                    return <option value={index} selected={index === 0}>{ sensor.sensor_model_name }</option>
                 })
             }
         </select>
